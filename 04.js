13579dbbb00aff634a758dd56fa52e7aee059db1
vefa <44091688+veefa@users.noreply.github.com>
2023-03-01 15:36:53 +0800

    Add files via upload

--- 04.js
+++ 04.js
@@ -3,23 +3,34 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
+
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  //Prop Collection
-  const togglerProps = {'aria-pressed': on, onClick: toggle};
-
-  return {on, toggle, togglerProps}
+  const getTogglerProps = ({ onClick, ...props }) => ({
+    'aria-pressed': on,
+    onClick: () => {
+      toggle();
+      onClick && onClick();
+    },
+    ...props,
+  });
+  return {on, toggle, getTogglerProps}
 }
-
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({on})} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+      >
         {on ? 'on' : 'off'}
       </button>
     </div>
